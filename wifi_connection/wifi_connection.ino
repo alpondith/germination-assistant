c791af4a8a4adb5bc28a6a62e3365049dc891108
alpondith <sujonalimran@gmail.com>
2022-10-13 15:55:43 +0600

    update

--- wifi_connection/wifi_connection.ino
+++ wifi_connection/wifi_connection.ino
@@ -1,59 +1,83 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
-  
+#include <WiFi.h>;
+#include <HTTPClient.h>;
+
+String url = "https://ga.insoulit.com/api/data";
+
+
+// wifi connection variables 
 const char* ssid = "black_sky";
 const char* password =  "blackmirror";
+unsigned long previousMillis = 0;
+unsigned long interval = 10000;
   
 void setup() {
   
   Serial.begin(115200);
-  delay(4000);   //Delay needed before calling the WiFi.begin
-  
-  WiFi.begin(ssid, password); 
-  
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  
-  Serial.println("Connected to the WiFi network");
+  initWiFi();
+  Serial.println("Setup done");
   
 }
   
 void loop() {
+
+  checkWifiConnection();
+
+  WiFiClient client;
+  HTTPClient http;   
   
- if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+  http.begin(client , url);
+//  http.addHeader("Content-Type", "application/json");   
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.addHeader("Accept", "application/json");
+//
+//  const size_t CAPACITY = JSON_OBJECT_SIZE(1);
+//  StaticJsonDocument<CAPACITY> doc ;
+
+
+
+  String httpRequestData = "device_key=xuHUr5wJI04O0A4tRKTT";        
+  httpRequestData = httpRequestData + "&T=39";
+  httpRequestData = httpRequestData + "&H=39";  
+  httpRequestData = httpRequestData + "&L=39";  
+  httpRequestData = httpRequestData + "&M=39";  
+  httpRequestData = httpRequestData + "&P=39"; 
+  Serial.println(httpRequestData); 
+  int httpResponseCode = http.POST(httpRequestData);
   
-   HTTPClient http;   
+  Serial.print("HTTP Response code : ");
+  Serial.println(httpResponseCode);
   
-//   http.begin("http://jsonplaceholder.typicode.com/posts");  
-   http.begin("https://ga.alpondith.dev/api/data");
-   http.addHeader("Content-Type", "text/plain");             
+
+  // Free resources
+  http.end();
   
-   int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
   
-   if(httpResponseCode>0){
   
-    String response = http.getString();                       //Get the response to the request
+  delay(8000);  //Send a request every 10 seconds
   
-    Serial.println(httpResponseCode);   //Print return code
-    Serial.println(response);           //Print request answer
-  
-   }else{
-  
-    Serial.print("Error on sending POST: ");
-    Serial.println(httpResponseCode);
-  
-   }
-  
-   http.end();  //Free resources
-  
- }else{
-  
-    Serial.println("Error in WiFi connection");   
-  
- }
-  
-  delay(10000);  //Send a request every 10 seconds
-  
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println("Congratulations! Connected to WiFi.");
+  Serial.print("IP : ");
+  Serial.println(WiFi.localIP());
+}
+
+void checkWifiConnection(){
+  unsigned long currentMillis = millis();
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.print(millis());
+    Serial.println(" : Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
+  }
 }
